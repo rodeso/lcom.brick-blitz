998b309db0985da399a78583b12d4d9efba16634
Rodrigo de Sousa <rodrigodiasdesousa@gmail.com>
2024-04-23 15:30:17 +0100

    aa

--- lab5/graphics.c
+++ lab5/graphics.c
@@ -99,7 +99,7 @@ int (vbe_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height
 }
 
 
-int (print_xpm)(xpm_map_t xpm, uint16_t x, uint16_t y) {
+int (vbe_draw_xpm)(xpm_map_t xpm, uint16_t x, uint16_t y) {
 
   xpm_image_t img;
 
