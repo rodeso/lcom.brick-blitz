2fd33d105c51e86d913c1d3a19da97a7d25b3a36
Rodrigo de Sousa <rodrigodiasdesousa@gmail.com>
2024-05-31 18:35:07 +0100

    mouse done

--- lab4/mouse.c
+++ lab4/mouse.c
@@ -1,11 +1,10 @@
 #include <lcom/lcf.h>
 #include "ps2.h" 
-#include "kbc.h"
 
 
 int hook_id3=0;
 uint8_t response=0;
-uint8_t bytes[3];
+uint8_t mouse_bytes[3];
 uint8_t byte_index=0;
 
 struct packet mouse_packet;
@@ -16,50 +15,55 @@ int (mouse_write_data)(uint8_t command){
   uint8_t mouse_response;
   do {
     attempts--;
-    if (write_command(0x64, 0xD4)) return 1;              // Ativar do modo D4 do i8042
-    if (write_command(0x60, command)) return 1;           // O comando para o rato é escrito na porta 0x60
+    if (write_command(INPUT_BUFFER, MOUSE_COMMAND_HEADS_UP)) return 1;              // Ativar do modo D4 do i8042
+    if (write_command(OUTPUT_BUFFER, command)) return 1;           // O comando para o rato é escrito na porta 0x60
     tickdelay(micros_to_ticks(20000));                        // Esperar alguns milissegundos
-    if (util_sys_inb(0x60, &mouse_response)) return 1;        // Ler a resposta da porta do output buffer
-    if (mouse_response == 0xFA) return 0;                      // Se a resposta for ACK, interromper o ciclo
-  } while (mouse_response != 0xFA && attempts);       
+    if (util_sys_inb(OUTPUT_BUFFER, &mouse_response)) return 1;        // Ler a resposta da porta do output buffer
+    if (mouse_response == ACK) return 0;                      // Se a resposta for ACK, interromper o ciclo
+  } while (mouse_response != ACK && attempts);       
 
  return 1;
 }
 
 
 void (mouse_ih)() {
-    read_KBC_outbuf(OUTPUT_BUFFER,&response,1);
+    read_scancode(OUTPUT_BUFFER,&response);
+    mouse_sync_bytes();
+    if (byte_index==3) { 
+      mouse_bytes_to_packet();
+      byte_index=0;
+    }
 }
 
 void (mouse_sync_bytes)(){
   if ((byte_index==0) && (response & BIT(3))){
     byte_index++;
-    bytes[0]=response;
+    mouse_bytes[0]=response;
   }
   else if (byte_index>0){
-     bytes[byte_index]=response;
+     mouse_bytes[byte_index]=response;
     byte_index++;
   }
 }
 
 void (mouse_bytes_to_packet)(){
     for (int i = 0 ; i < 3 ; i++) {
-    mouse_packet.bytes[i] = bytes[i];
+    mouse_packet.bytes[i] = mouse_bytes[i];
   }
-  mouse_packet.lb = bytes[0] & BIT(0);
-  mouse_packet.mb = bytes[0] & BIT(2);
-  mouse_packet.rb = bytes[0] & BIT(1);
-  mouse_packet.x_ov = bytes[0] & BIT(6);
-  mouse_packet.y_ov = bytes[0] & BIT(7);
-  mouse_packet.delta_x = (bytes[0] & BIT(4)) ? (0xFF00 | bytes[1]) : bytes[1];
-  mouse_packet.delta_y = (bytes[0] & BIT(5)) ? (0xFF00 | bytes[2]) : bytes[2];
+  mouse_packet.lb = mouse_bytes[0] & BIT(0);
+  mouse_packet.mb = mouse_bytes[0] & BIT(2);
+  mouse_packet.rb = mouse_bytes[0] & BIT(1);
+  mouse_packet.x_ov = mouse_bytes[0] & BIT(6);
+  mouse_packet.y_ov = mouse_bytes[0] & BIT(7);
+  mouse_packet.delta_x = (mouse_bytes[0] & BIT(4)) ? (0xFF00 | mouse_bytes[1]) : mouse_bytes[1];
+  mouse_packet.delta_y = (mouse_bytes[0] & BIT(5)) ? (0xFF00 | mouse_bytes[2]) : mouse_bytes[2];
 }
 
 int (mouse_subscribe_int)(uint8_t *bit_no) {
 
     if(bit_no==NULL) {return 1;}
     
-    hook_id3=KEYBOARD_IRQ;
+    hook_id3=IRQ_LINE_MOUSE;
     *bit_no=hook_id3;
 
     int pol = sys_irqsetpolicy(IRQ_LINE_MOUSE,IRQ_REENABLE|IRQ_EXCLUSIVE,&hook_id3);
--- lab4/ps2.h
+++ lab4/ps2.h
@@ -10,10 +10,14 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define IRQ_LINE_MOUSE 12
-#define INPUT_BUFFER 0x64 //sending KBC commands and arguments
-#define OUTPUT_BUFFER 0x60 //receive acknowledgements and responses
-#define MOUSE_COMMAND_HEADS_UP 0xD4; //Request forwarding of byte (command) to the mouse
+#define IRQ_LINE_MOUSE 12 /**< Mouse IRQ line */
+#define INPUT_BUFFER 0x64 /**< Sending KBC commands and arguments */
+#define OUTPUT_BUFFER 0x60 /**< Receive acknowledgements and responses */
+#define MOUSE_COMMAND_HEADS_UP 0xD4 /**< Request forwarding of byte (command) to the mouse */
+#define ACK 0xFA /**< Acknowledgement byte */
+#define SET_STREAM_MODE 0xEA /**< Set stream mode command */
+#define ENABLE_DATA_REPORTING 0xF4 /**< Enable data reporting command */
+#define DISABLE_DATA_REPORTING 0xF5 /**< Disable data reporting command */
 
 #define yippee
 
